bda536a5c1f4d42cba30e695c6d553a95e184f05
shravan <shravandas191@gmail.com>
2026-06-09 12:01:29 +0530

    vercel changes

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/tasks'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
