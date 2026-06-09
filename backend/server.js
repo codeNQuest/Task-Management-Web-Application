e07599afa910601f83cbb06056cd94f1d749f625
shravan <shravandas191@gmail.com>
2026-06-09 12:39:34 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const dotenv = require('dotenv');
-const cors = require('cors');
 const connectDB = require('./config/db');
 
 dotenv.config({ path: path.join(__dirname, '.env') });
